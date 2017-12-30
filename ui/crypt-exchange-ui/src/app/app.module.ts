6adad17e12b982c26d2855172ed53dc9f1a2b473
Abhinendra <abhinendra.singh22@gmail.com>
2017-12-30 17:47:00 +0530

    Adding Dashboard

--- ui/crypt-exchange-ui/src/app/app.module.ts
+++ ui/crypt-exchange-ui/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import{DashboardModule} from './dashboard/dashboard.module'
+import {AppComponent} from './app.component';
 
 
 @NgModule({
@@ -10,9 +10,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule.forRoot(),
+    BrowserModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ ui/crypt-exchange-ui/src/app/dashboard/currency-table/currency-table.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CurrencyTableComponent } from './currency-table.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CurrencyTableComponent]
+})
+export class CurrencyTableModule { }
